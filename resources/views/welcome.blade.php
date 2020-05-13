a33cd8039969baae4f63b2ea15ee4a99704d8dfa
maxsandrosarat <msaratribeiro@gmail.com>
2020-05-12 22:57:24 -0400

    rollback

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -266,7 +266,7 @@
                     @endauth
             @endif
             <div class="container">
-                <nav class="navbar navbar-expand-lg navbar-light bg-primary rounded">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
                       <span class="navbar-toggler-icon"></span>
                     </button>
